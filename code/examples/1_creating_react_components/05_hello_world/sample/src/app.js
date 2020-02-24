c57e0a93e242f491ea2d685aaf37d1b8d87564ca
Venkat Subramaniam <venkats@agiledeveloper.com>
2020-02-24 11:49:05 -0900

    hello world

---
+++ code/examples/1_creating_react_components/05_hello_world/sample/src/app.js
@@ -0,0 +1,7 @@
+import React, { Component, createElement } from 'react';
+
+export default class App extends Component {
+  render() {
+    return createElement('div', null, "Hello world!");
+  }
+}
