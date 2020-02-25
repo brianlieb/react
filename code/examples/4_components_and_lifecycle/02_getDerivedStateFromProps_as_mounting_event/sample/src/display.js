2a974b18c0a8aebab9fac00d7f9a56bef38b1905
Venkat Subramaniam <venkats@agiledeveloper.com>
2020-02-25 14:55:37 -0900

    getDerivedStateFromProps as mounting event

---
+++ code/examples/4_components_and_lifecycle/02_getDerivedStateFromProps_as_mounting_event/sample/src/display.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Display extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {};
+    
+    console.log('constructor called...');
+  }
+  
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps called...');
+    return null;
+  }
+  
+  render() {
+    return <div>Count: { this.props.count }</div>;
+  }
+}
