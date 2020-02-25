43d4fe885dc7d3412d1f04f88778ee5d3f9b7a8d
Venkat Subramaniam <venkats@agiledeveloper.com>
2020-02-25 13:28:07 -0900

    bad ideas persist

---
+++ code/examples/3_working_with_state_and_events/14_bad_ideas_persist/sample/src/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  process(e) {
+    e.persist(); //e.badIdea();
+    
+    console.log(`You clicked on ${e.target.textContent}`);
+    
+    setTimeout(() => console.log(`You clicked on ${e.target.textContent}`), 2000);
+  }
+  
+  render() {
+    return <div>
+      <span onClick={ e => this.process(e) }>Hello</span>
+      <p>Click on the word Hello</p>
+      <p>This code fails because we are trying to use the event object
+      after it has been released - event pooling</p>
+    </div>;
+  }
+}
