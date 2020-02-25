fc233a18d9bf208eebe328318a170df53264fc38
Venkat Subramaniam <venkats@agiledeveloper.com>
2020-02-24 15:29:50 -0900

    solution

---
+++ code/labs/1_creating_react_components/lab2/solution/lab/src/lab.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(React.createElement(App, null, null),
+  document.getElementById('contents'));
