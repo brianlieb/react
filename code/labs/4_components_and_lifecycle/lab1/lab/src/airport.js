0ef8ec9fb9d291f81dc8791da2c9daeff544e97b
Brian Lieb <brianlieb@hotmail.com>
2020-02-26 09:03:43 -0900

    formatting

--- code/labs/4_components_and_lifecycle/lab1/lab/src/airport.js
+++ code/labs/4_components_and_lifecycle/lab1/lab/src/airport.js
@@ -1,7 +1,7 @@
 export default class Airport {
   constructor(code, name, temperature, delay) {
     this.code = code;
-    this.name = name;                
+    this.name = name;
     this.temperature = temperature;
     this.delay = delay;
   }
