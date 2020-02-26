f56200b91466a0df2273d84a9cd076f035970db3
Venkat Subramaniam <venkats@agiledeveloper.com>
2020-02-26 11:58:35 -0900

    solution

---
+++ code/labs/5_creating_validators/solution/lab/src/display-airport-status.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Airport from './airport';
+
+export default class DisplayAirportStatus extends Component {
+  extractAndSortAirport(airports, delayed) {
+    const byCode = (airport1, airport2) => airport1.code.localeCompare(airport2.code);
+    
+    return airports.filter(airport => airport.delay === delayed)
+      .sort(byCode);
+  }
+  
+  render() {
+    
+    const airports = this.extractAndSortAirport(this.props.data, this.props.delayed);      
+    
+    return <div>
+      <h2>{ this.props.title }</h2>
+      <table>
+        <tr><th>Code</th><th>Name</th><th>Temperature</th></tr>
+        {
+          airports.map(airport => <tr key={ airport.code }><td>{ airport.code }</td><td>{ airport.name }</td><td>{ airport.temperature }</td></tr>)
+        }
+      </table>
+    </div>;
+  }
+}
+
+DisplayAirportStatus.defaultProps = {
+  title: 'Please Set A Title',
+};
+
+DisplayAirportStatus.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.instanceOf(Airport)),
+  delayed: PropTypes.bool,
+  title: function(props, key, component) {
+    if(props[key].split(' ')
+      .every(word => /[A-Z]/.test(word[0]))) {
+      return null;
+    }
+    
+    return new Error(
+      `Each word of ${key} for ${component} should be capitalized`);
+  }
+};
